8c3557867ef43b69ffa7c0ded209064c5845ac90
Tammy Do <tammytdo11@gmail.com>
2019-02-07 01:10:28 -0800

    scaffolding assignment

---
+++ js/app_demo.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/*
+ask for username, fact, and movie
+give a response that lets the user know whats going on
+I want a name
+i want to see if they match my quirky fact
+if they say im cool, say something nice
+or else ...
+
+
+*/
+var username, quirkyfact, amICool;
+
+username = prompt('what is your name');
+
+console.log(typeof(username));
+
+if(username === ''){
+  username = prompt('I really want to know your name');
+}
+
+if(username === ''){
+  username = 'noname person';
+}
+
+alert('hello ' + username);
+
+quirkyfact = prompt('tell me a quirky fact about yourself');
+
+if(quirkyfact === 'dnd'){
+  alert('that is my favorite thing too');
+} else {
+  alert('oh, ' + quirkyfact + ' sounds really cool, tell me more');
+}
